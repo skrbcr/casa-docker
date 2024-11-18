09f8f9dbb6fedb0ab01fbfefe058492d07e24dd2
skrbcr <90087433+skrbcr@users.noreply.github.com>
2024-11-18 21:27:16 +0900

    Update source/startup.py

--- source/startup.py
+++ source/startup.py
@@ -2,7 +2,8 @@ import sys
 import os
 try:
     sys.path.append(os.path.expanduser('~') + '/analysis_scripts')
-    sys.path.append('/github/home/analysis_scripts')
+    # sys.path.append('/github/home/analysis_scripts')
 except Exception as e:
     pass
 import analysisUtils as au
+print('Startup script executed')
